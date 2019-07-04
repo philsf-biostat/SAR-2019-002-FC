a9cbaf80a92e67fb9c6ae979512372722d01e7ef
Felipe Figueiredo <philsf79@gmail.com>
2019-07-04 17:20:22 -0300

    variáveis não-categóricas avaliadas com correlação de Spearman

--- scripts/assoc-vitiqol.R
+++ scripts/assoc-vitiqol.R
@@ -16,9 +16,9 @@ library(ggplot2, quietly = TRUE)
 # "TempoDoenca")
 
 vitiqol.kw.sexo <- kruskal.test(dados$`Escore VitiQoL - Máx 90`, dados$Sexo)
-vitiqol.kw.idade <- kruskal.test(dados$`Escore VitiQoL - Máx 90`, dados$Idade)
+vitiqol.cor.idade <- cor.test(dados$`Escore VitiQoL - Máx 90`, dados$Idade, method = "sp")
 vitiqol.kw.fototipo <- kruskal.test(dados$`Escore VitiQoL - Máx 90`, dados$Fototipo)
-vitiqol.kw.asc <- kruskal.test(dados$`Escore VitiQoL - Máx 90`, dados$ASC)
+vitiqol.cor.asc <- cor.test(dados$`Escore VitiQoL - Máx 90`, dados$ASC, method = "sp")
 vitiqol.kw.tratamento <- kruskal.test(dados$`Escore VitiQoL - Máx 90`, dados$Tratamento)
 vitiqol.kw.tempodoenca <- kruskal.test(dados$`Escore VitiQoL - Máx 90`, dados$TempoDoenca)
 
