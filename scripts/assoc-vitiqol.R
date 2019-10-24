aa8727ebb9b484d1be5166f671b48df4fa0c371b
Felipe Figueiredo <philsf79@gmail.com>
2019-10-24 17:38:31 -0300

    cola mediana/IQR incluída

--- scripts/assoc-vitiqol.R
+++ scripts/assoc-vitiqol.R
@@ -26,8 +26,9 @@ vitiqol.kw.laboral <- kruskal.test(dados$`Escore VitiQoL - Máx 90`, dados$Traba
 vitiqol.cor.idade <- cor.test(dados$`Escore VitiQoL - Máx 90`, dados$Idade, method = "sp")
 vitiqol.cor.asc <- cor.test(dados$`Escore VitiQoL - Máx 90`, dados$ASC, method = "sp")
 
-# associação com Sexo - resultados a declarar
-# print(CreateTableOne(vars = "vtq", strata = "Sexo", dados), nonnormal = TRUE)
+# associação com Sexo e Trabalho - resultados a declarar
+# print(CreateTableOne(vars = "Escore VitiQoL - Máx 90", strata = "Sexo", dados), nonnormal = TRUE)
+# print(CreateTableOne(vars = "Escore VitiQoL - Máx 90", strata = "Trabalho", dados), nonnormal = TRUE)
 
 ggplot(dados, aes(Fototipo, `Escore VitiQoL - Máx 90`)) +
   ylab("Escore VitiQoL") +
